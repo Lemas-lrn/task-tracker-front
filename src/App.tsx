991cfe1ca247ab4e1186c90bbedd567a43d79480
Lemas <llemas@yandex.ru>
2021-02-10 23:06:04 +0300

    fixed  component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/mainBody/header/Header";
+import Header from "./components/header/Header";
 import MainBody from "./components/mainBody/MainBody";
 function App() {
   return (
