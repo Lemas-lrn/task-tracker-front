6e08cd2f634eed8877c586d902ff19c6f821737e
Lemas <llemas@yandex.ru>
2021-02-14 11:35:20 +0300

    menu buttons type

--- src/components/header/menuButtons/ MenuButtons.tsx
+++ src/components/header/menuButtons/ MenuButtons.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
+import { Link as RouterLink } from "react-router-dom";
 
-const MenuButtons = () => {
-    return (
-        <div>MenuButtons</div>
-    )
+interface IData  {
+    
+    label: string,
+    href: string
+}
+
+type Props = {
+    data: Array<IData> ,
+}
+const MenuButtons: React.FC<Props> = ({data}) => {
+        return data.map(({ label, href }) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color: "inherit",
+            to: href,
+            component: RouterLink,
+          }}
+        >
+          {label}
+        </Button>
+      );
+    });
 }
 
 export default MenuButtons
